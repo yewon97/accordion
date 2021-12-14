d73b9915c7fdae8601a437ed1ebdfd1f8f7d2a57
yewon97 <dpdnjs9936@gmail.com>
2021-12-14 16:14:20 +0900

    아코디언 메뉴 수정&완성

--- js/accordion.js
+++ js/accordion.js
@@ -9,16 +9,16 @@ window.onload = () => {
   const btnAllClose = document.querySelector("#btn-all-close");
   
   // 반복문 순회하면서 해당 FAQ제목 클릭시 콜백 처리
-  for(let i=0; i<panelFaqContainer.length; i++) {
+  for( let i=0; i < panelFaqContainer.length; i++ ) {
     panelFaqContainer[i].addEventListener('click', function() { // 클릭시 처리할 일
       // FAQ 제목 클릭시 -> 본문이 보이게끔 -> active 클래스 추가
-      panelFaqAnswer[i].classList.add('active');
+      panelFaqAnswer[i].classList.toggle('active');
     });
   };
   
   btnAllClose.addEventListener('click', function() {
     // 버튼 클릭시 처리할 일  
-    for(let i=0; i<panelFaqAnswer.length; i++) {
+    for(let i=0; i < panelFaqAnswer.length; i++) {
         panelFaqAnswer[i].classList.remove('active');
     };
   });
