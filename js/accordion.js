7d0db7e824314bad33c3552561277dd7393df2b2
yewon97 <dpdnjs9936@gmail.com>
2021-12-14 15:08:52 +0900

    반복문 순회하면서 해당 FAQ 제목 클릭시 콜백 처리

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,13 @@
 window.onload = () => {
   const panelFaqContainer = document.querySelectorAll(".panel-faq-container");
   console.log(panelFaqContainer); // NodeList 객체
+
+
+  // 반복문 순회하면서 해당 FAQ제목 클릭시 콜백 처리
+  for(let i=0; i<panelFaqContainer.length; i++) {
+    panelFaqContainer[i].addEventListener('click', function() {
+      // 클릭시 처리할 일
+      console.log(`나 클릭... ${i}`);
+    });
+  }
 }
